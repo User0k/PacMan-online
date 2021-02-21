018890b27b84e609bb51dcaa45b792201f0cd588
User0k <glucus2@yandex.ru>
2021-02-21 18:04:10 +0300

    add gameBoard.js for drawing a board

---
+++ src/js/gameBoard.js
@@ -0,0 +1,26 @@
+import level from './levels';
+
+const COLUMNS = 20;
+const grid = document.querySelector('.grid');
+
+export default function createBoard() {
+  const squares = [];
+  for (let i = 0; i < level.length; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    grid.appendChild(square);
+    squares.push(square);
+
+    if(level[i] === 0) {
+      squares[i].classList.add('empty')
+    } else if (level[i] === 1) {
+      squares[i].classList.add('wall')
+    } else if (level[i] === 2) {
+      squares[i].classList.add('gold')
+    } else if (level[i] === 3) {
+      squares[i].classList.add('power-pill')
+    } else if(level[i] === 4) {
+      squares[i].classList.add('ghost-lair')
+    }
+  }
+};
