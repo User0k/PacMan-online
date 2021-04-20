a566986cc0ddcd13ac903f08bf8b15dfff2711de
User0k <glucus2@yandex.ru>
2021-04-20 21:00:18 +0300

    add dotEat function and score variables in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ import { COLUMNS, createBoard, squares } from './gameBoard';
 createBoard();
 let curLocation = 290;
 let nextSquare = curLocation;
+let score = 0;
+let scoreDisplay = document.querySelector('#score');
 const DOTSCORE = 10;
 const PILLSCORE = 50;
 const SCAREDSCORE = 200;
@@ -40,5 +42,13 @@ document.addEventListener('keydown', function(e) {
       squares[curLocation].classList.add('pacman');
     }
   }
-  console.log(nextSquare)
+  dotEat(curLocation);
 });
+
+function dotEat(location) {
+  if (squares[location].classList.contains('dot')) {
+    squares[location].classList.remove('dot');
+    score += DOTSCORE;
+    scoreDisplay.textContent = score;
+  }
+}
