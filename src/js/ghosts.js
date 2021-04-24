89d6767444df59302c09cf6422cacc5c2c7c471b
User0k <glucus2@yandex.ru>
2021-04-24 23:49:39 +0300

    create Ghost class in new file ghost.js

---
+++ src/js/ghosts.js
@@ -0,0 +1,9 @@
+class Ghost {
+  constructor(ghostName, ghostLocation) {
+    this.ghostName = ghostName;
+    this.ghostLocation = ghostLocation;
+    this.isScared = false;
+  }
+
+};
+export default Ghost;
