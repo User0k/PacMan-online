14ab781372b1998d690d9a3fce2a087fd8ce15a2
User0k <glucus2@yandex.ru>
2021-04-27 21:57:07 +0300

    add scare method in Ghost class

--- src/js/ghosts.js
+++ src/js/ghosts.js
@@ -1,3 +1,6 @@
+import { squares } from './gameBoard';
+import { scaredTimer, unscare } from './index';
+
 class Ghost {
   constructor(ghostName, ghostLocation) {
     this.ghostName = ghostName;
@@ -5,5 +8,11 @@ class Ghost {
     this.isScared = false;
   }
 
+  scare() {
+    this.isScared = true;
+    squares[this.ghostLocation].classList.add('scared-ghost');
+    squares[this.ghostLocation].classList.remove('ghost');
+    setTimeout(unscare, scaredTimer);
+  }
 };
 export default Ghost;
