12c00cb7d23a5c90250830ced738b0eed8594f2e
User0k <glucus2@yandex.ru>
2021-05-02 20:11:56 +0300

    add tryMove method for Ghost

--- src/js/ghosts.js
+++ src/js/ghosts.js
@@ -1,11 +1,25 @@
-import { squares } from './gameBoard';
+import { squares, COLUMNS } from './gameBoard';
 import { scaredTimer, unscare } from './index';
 
 class Ghost {
   constructor(ghostName, ghostLocation) {
     this.ghostName = ghostName;
     this.ghostLocation = ghostLocation;
+    this.nextLocation = ghostLocation;
     this.isScared = false;
+    this.speed = 400;
+    this.directions = [-1, -COLUMNS, 1, COLUMNS];
+  }
+
+  tryMove() {
+    this.nextLocation = this.ghostLocation + this.directions[Math.floor(Math.random() * this.directions.length)];
+    if (squares[this.nextLocation].classList.contains('wall')) this.nextLocation = this.ghostLocation;
+    else {
+      squares[this.ghostLocation].classList.remove(this.ghostName, 'ghost', 'scared-ghost');
+      this.ghostLocation = this.nextLocation;
+      squares[this.ghostLocation].classList.add(this.ghostName, 'ghost');
+      if(this.isScared == true) squares[this.ghostLocation].classList.add('scared-ghost');
+    }
   }
 
   scare() {
