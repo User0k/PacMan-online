4334d75f18aa1f502194d2fb51d7b5a36fbc342a
User0k <glucus2@yandex.ru>
2021-06-05 14:15:15 +0300

    renew webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,58 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { LoaderOptionsPlugin } = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports = {
-    entry: {
-      main: path.resolve(__dirname, './src/js/index.js'),
-    },
-    devServer: {
-      historyApiFallback: true,
-      contentBase: path.resolve(__dirname, './dist'),
-      open: true,
-      compress: true,
-      hot: true,
-      port: 8080,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Pacman online',
-        template: path.resolve(__dirname, './src/index.html'), //template.html
-        filename: 'index.html',
-      }),
-/*      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: './src/img',
-            to: './img',
-          }
-        ]
-      }), */
-      new MiniCssExtractPlugin({ filename: 'styles.css'  }),
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: path.resolve(__dirname, './src/js/index.js'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Pacman online',
+      template: path.resolve(__dirname, './src/index.html'), //template.html
+      filename: 'index.html',
+    }),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: ['babel-loader'],
-        },
-        {
-          test: /\.css$/,
-          use: [
-             MiniCssExtractPlugin.loader,
-             { loader: 'css-loader' }
-          ],
-        },
-        {
-          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-          type: 'asset/resource',
-        },
-      ],
-    },
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: '[name].js',
-    },
-}
+  },
+};
