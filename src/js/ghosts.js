3d4a889449ead412c112b9bb8c9739abad37fa1b
User0k <glucus2@yandex.ru>
2021-06-07 23:27:55 +0300

    add freeze ability for ghosts.js

--- src/js/ghosts.js
+++ src/js/ghosts.js
@@ -7,6 +7,7 @@ class Ghost {
     this.ghostLocation = ghostLocation;
     this.nextLocation = ghostLocation;
     this.isScared = false;
+    this.freeze = false;
     this.speed = 400;
     this.directions = [-1, -COLUMNS, 1, COLUMNS];
   }
@@ -14,7 +15,8 @@ class Ghost {
   tryMove() {
     this.nextLocation = this.ghostLocation + this.directions[Math.floor(Math.random() * this.directions.length)];
     if (squares[this.nextLocation].classList.contains('wall') ||
-        squares[this.nextLocation].classList.contains('ghost'))
+        squares[this.nextLocation].classList.contains('ghost') ||
+        this.freeze === true)
         this.nextLocation = this.ghostLocation;
     else {
       squares[this.ghostLocation].classList.remove(this.ghostName, 'ghost', 'scared-ghost');
@@ -22,7 +24,7 @@ class Ghost {
       squares[this.ghostLocation].classList.add(this.ghostName, 'ghost');
       if(this.isScared == true) squares[this.ghostLocation].classList.add('scared-ghost');
       this.meetPacman();
-    }
+    };
   }
 
   scare() {
