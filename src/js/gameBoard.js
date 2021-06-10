6deb3407d2d2efe3d4a7050155b3ab4433da1f91
User0k <glucus2@yandex.ru>
2021-06-10 22:04:57 +0300

    rewrite gameboard to a new class

--- src/js/gameBoard.js
+++ src/js/gameBoard.js
@@ -1,26 +1,43 @@
 import level from './levels';
-
-const COLUMNS = 20;
-const grid = document.querySelector('.grid');
-grid.style.width = `${COLUMNS * 20}px`;
+export const COLUMNS = 20;
 export const squares = [];
 
-export const createBoard = function() {
-  for (let i = 0; i < level.length; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    grid.appendChild(square);
-    squares.push(square);
-    if(level[i] === 0) {
-      squares[i].classList.add('empty')
-    } else if (level[i] === 1) {
-      squares[i].classList.add('wall')
-    } else if (level[i] === 2) {
-      squares[i].classList.add('gold')
-    } else if (level[i] === 3) {
-      squares[i].classList.add('power-pill')
-    } else if(level[i] === 4) {
-      squares[i].classList.add('ghost-lair')
-    }
+class Board {
+  constructor() {
+    this.levelNumber = 0;
+    this.curLevel = level[0];
+    this.wrapper = document.getElementById('wrapper');
+  }
+
+  createBoard() {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    grid.style.width = `${COLUMNS * 20}px`;
+    for (let i = 0; i < this.curLevel.length; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      grid.appendChild(square);
+      squares.push(square);
+      if(this.curLevel[i] === 0) {
+        squares[i].classList.add('empty', `${i}`)
+      } else if (this.curLevel[i] === 1) {
+        squares[i].classList.add('wall', `${i}`)
+      } else if (this.curLevel[i] === 2) {
+        squares[i].classList.add('dot', `${i}`)
+      } else if (this.curLevel[i] === 3) {
+        squares[i].classList.add('power-pill', `${i}`)
+      } else if(this.curLevel[i] === 4) {
+        squares[i].classList.add('ghost-lair', `${i}`)
+      };
+    };
+    this.wrapper.appendChild(grid);
+  }
+
+  clearBoard() {
+    for (let i = 0; i < squares.length; i++) {
+      squares[i].className = 'square';
+    };
   }
 };
+
+export default Board;
