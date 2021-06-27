da5413e12d12dbcbc3b1e96d74554be7df47c92c
User0k <glucus2@yandex.ru>
2021-06-27 14:54:19 +0300

    a ghost can appear from another side of the grid if it does not have a wall

--- src/js/ghosts.js
+++ src/js/ghosts.js
@@ -9,11 +9,12 @@ class Ghost {
     this.isScared = false;
     this.freeze = false;
     this.speed = globalSpeed / 3.5;
-    this.directions = [-1, -COLUMNS, 1, COLUMNS];
+    this.directions = [1, -COLUMNS, -1, COLUMNS];
   }
 
   tryMove() {
     this.nextLocation = this.ghostLocation + this.directions[Math.floor(Math.random() * this.directions.length)];
+    this.walkThroughGrid();
     if (squares[this.nextLocation].classList.contains('wall') ||
         squares[this.nextLocation].classList.contains('ghost') ||
         this.freeze === true)
@@ -22,9 +23,17 @@ class Ghost {
       squares[this.ghostLocation].classList.remove(this.ghostName, 'ghost', 'scared-ghost');
       this.ghostLocation = this.nextLocation;
       squares[this.ghostLocation].classList.add(this.ghostName, 'ghost');
-      if(this.isScared == true) squares[this.ghostLocation].classList.add('scared-ghost');
+      if(this.isScared === true) squares[this.ghostLocation].classList.add('scared-ghost');
       this.meetPacman();
     };
+  }
+
+  //this method will replace the ghost to another side of the board if the ghost has reached it and still moving
+  walkThroughGrid() {
+    if (this.ghostLocation % COLUMNS === 19 && this.nextLocation === this.ghostLocation + 1)
+      this.nextLocation = this.ghostLocation - COLUMNS + 1;
+    if (this.ghostLocation % COLUMNS === 0 && this.nextLocation === this.ghostLocation - 1)
+      this.nextLocation = this.ghostLocation + COLUMNS - 1;
   }
 
   scare() {
