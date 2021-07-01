ef80c3613a082ec50ea7ef131a445cd92468cb3e
User0k <glucus2@yandex.ru>
2021-07-01 21:23:22 +0300

    reexport sounds from 1 file

---
+++ src/sounds/index.js
@@ -0,0 +1,8 @@
+import soundBeginning from './beginning.wav';
+import soundDeath from './death.wav';
+import soundDot from './dot.wav';
+import soundEatGhost from './eatGhost.wav';
+import soundGameOver from './gameOver.wav';
+import soundPowerPill from './powerPill.wav';
+
+export { soundBeginning, soundDeath, soundDot, soundEatGhost, soundGameOver, soundPowerPill };
